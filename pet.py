2971e009c4d82c76d6bf9dde27b327678cbb9830
ratue1256 <ratue1256@gmail.com>
2026-08-20 02:14:13 +0200

    fix root qui se bloque en haut de l ecran

--- pet.py
+++ pet.py
@@ -23,13 +23,18 @@ class GUITHREADINFO(ctypes.Structure):
         ("rcCaret", wintypes.RECT)
     ]
 
+virt_x = ctypes.windll.user32.GetSystemMetrics(76)
+virt_y = ctypes.windll.user32.GetSystemMetrics(77)
+virt_w = ctypes.windll.user32.GetSystemMetrics(78)
+virt_h = ctypes.windll.user32.GetSystemMetrics(79)
+
 def get_exact_typing_pos():
     try:
         gui = GUITHREADINFO(cbSize=ctypes.sizeof(GUITHREADINFO))
         if ctypes.windll.user32.GetGUIThreadInfo(0, ctypes.byref(gui)) and gui.hwndCaret:
             pt = wintypes.POINT(gui.rcCaret.left, gui.rcCaret.top)
             ctypes.windll.user32.ClientToScreen(gui.hwndCaret, ctypes.byref(pt))
-            if pt.x > -1000 and pt.y > -500:
+            if pt.x > -2000 and pt.y > virt_y:
                 return pt.x, pt.y
     except Exception:
         pass
@@ -38,20 +43,16 @@ def get_exact_typing_pos():
     ctypes.windll.user32.GetCursorPos(ctypes.byref(pt))
     return pt.x, pt.y
 
-virt_x = ctypes.windll.user32.GetSystemMetrics(76)
-virt_y = ctypes.windll.user32.GetSystemMetrics(77)
-virt_w = ctypes.windll.user32.GetSystemMetrics(78)
-virt_h = ctypes.windll.user32.GetSystemMetrics(79)
-
 class RootPet:
     def __init__(self, fenetre):
         self.fenetre = fenetre
         self.taille_sprite = 90
         self.largeur_fen = 340
         self.hauteur_fen = 140
+        self.y_sol = virt_y + virt_h - self.hauteur_fen - 60
         
         self.pos_x = virt_x + random.randint(200, max(300, virt_w - 400))
-        self.pos_y = virt_y + virt_h - self.taille_sprite - 80
+        self.pos_y = self.y_sol
         self.direction = 1
         self.etat = "marche"
         
@@ -111,7 +112,7 @@ class RootPet:
         sx, sy = get_exact_typing_pos()
         
         self.souris_memo_x = min(max(virt_x + 20, sx - (self.largeur_fen // 2)), virt_x + virt_w - self.largeur_fen - 20)
-        self.souris_memo_y = min(max(virt_y + 20, sy - self.taille_sprite - 20), virt_y + virt_h - self.hauteur_fen - 50)
+        self.souris_memo_y = min(max(virt_y + 40, sy - self.taille_sprite - 20), self.y_sol)
         
         self.cible_x = self.souris_memo_x
         self.cible_y = self.souris_memo_y
@@ -128,6 +129,11 @@ class RootPet:
                 self.direction = -1
             elif self.pos_x <= virt_x + 20:
                 self.direction = 1
+                
+            if self.pos_y < self.y_sol:
+                self.pos_y += min(4, self.y_sol - self.pos_y)
+            elif self.pos_y > self.y_sol:
+                self.pos_y = self.y_sol
                 
             if random.random() < 0.012:
                 self.etat = "pause"
@@ -187,6 +193,9 @@ class RootPet:
                 self.label_texte.configure(text=f"{self.faute_en_cours['texte_corrige']}", fg="#ffffff")
                 self.fenetre.after(1600, self.finir_remise)
                 
+        if self.etat not in ("fuite_hors_ecran", "cache"):
+            self.pos_y = min(max(virt_y + 30, self.pos_y), self.y_sol)
+            
         self.fenetre.geometry(f"{self.largeur_fen}x{self.hauteur_fen}+{int(self.pos_x)}+{int(self.pos_y)}")
         
         frames = self.frames_droite if self.direction == 1 else self.frames_gauche
