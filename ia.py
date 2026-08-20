c67b69150c9cc6fb5a6c4072b4bb2272e1cf66f5
ratue1256 <ratue1256@gmail.com>
2026-08-20 16:54:38 +0200

    suppression de la liste des mots ignores

--- ia.py
+++ ia.py
@@ -3,13 +3,6 @@ from spellchecker import SpellChecker
 # dico fr pour chopper les fautes
 spell = SpellChecker(language="fr")
 
-# les mots a surtout pas toucher sinon ca va casser les pseudos et les jeux
-IGNORED_WORDS = {
-    "osu", "ezio", "roblox", "discord", "steam", "valorant", "minecraft",
-    "git", "github", "commit", "push", "pull", "add", "fix", "wip", "merge",
-    "aywen", "root", "professeur", "python", "dev", "bug", "bugs", "fps", "tk"
-}
-
 def clean_word(w):
     # vire la ponctuation qui traine autour
     return w.lower().strip(".,!?:;\"'()-_/[]{}~`")
@@ -26,8 +19,8 @@ def check_sentence_errors(raw_sentence):
     for w in words:
         clean = clean_word(w)
         
-        # si y a une apostrophe, si c est dans la liste ou si le mot est bon on touche pas
-        if "'" in w or len(clean) <= 2 or clean in IGNORED_WORDS or clean in spell:
+        # si y a une apostrophe, mot trop court ou si le mot existe dans le dico on touche pas
+        if "'" in w or len(clean) <= 2 or clean in spell:
             corrected_list.append(w)
             continue
             
